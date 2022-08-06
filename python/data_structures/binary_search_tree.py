d7064a44b8628c29f81231a66e2d20c17b0835aa
gina305 <napier.gina@gmail.com>
2022-08-06 18:15:04 -0400

    Updayed BST.py code

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -19,10 +19,52 @@ Argument: value
 Returns: boolean indicating whether or not the value is in the tree at least once.
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+   
+    def __init__(self, data=None):
+        self.root = data
+        self.left = None
+        self.right = None
+        self.value = "word"
+        return self.root     
+        
+    def add(self, word):
+        # if self.left == None:
+        #     self.left = BinaryTree(word)
+        # # else:
+        # #     node_data = BinaryTree(value)
+        # #     node_data.left = self.left
+        # #     self.left = node_data
+        # # return self.root
+        # print(self.left)
+        # return self.left
+        word = word.split()
 
-    def some_method(self):
-        # method body here
-        pass
+        return word
+
+    def insert_right(self, value):
+        if self.right == None:
+            self.right = BinaryTree(value)
+        else:
+            node_data = BinaryTree(value)
+            node_data.right = self.right
+            self.right = node_data
+        return self.right
+   
+
+    # def get_left(self):
+    #     return self.left
+
+    # def get_right(self):
+    #     return self.right
+
+    # def set_left(self, tree):
+    #     self.left = tree
+
+    # def set_right(self, tree):
+    #     self.right = tree
+
+    # def set_data(self, data):
+    #     self.data = data
+
+    # def get_data(self):
+    #     return self.data
