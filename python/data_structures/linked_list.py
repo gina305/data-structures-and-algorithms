be38788a09b85e244aff7608f4ee23cd59b07c7b
gina305 <napier.gina@gmail.com>
2022-08-12 11:50:36 -0400

    complete challenge 7

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,41 +1,111 @@
-class Node:
-   def __init__(self, value=None):
-      self.value = value
-      self.next = None
-
-
-
+#Collaborator(s) Liesl White - see readme
 class LinkedList:
-    """
-    Create a Linked List class with an insert, includes and 'to string' methods
-    """
-
     def __init__(self):
         self.head = None
 
-    # insertion method for the linked list
-    def insert(self, value):
-
-        #Create a new node
-        newNode = Node(value)
-
-        #If there is a node already with the head value, set the next value to the new node
-        if (self.head):
-            current = self.head #head attribute value
-            while (current.next):
-                current = current.next
-            current.next = newNode
-        else:
-            #Otherwise give the new node the "head" attribute
-            self.head = newNode
-
-
     def __str__(self):
-        #return self.data
-        try:
-            return self.value
-        except (AttributeError):
-            return 'NULL'
+        input_ = ""
+        current = self.head
 
-class TargetError:
+        while current:
+            input_ += f"{{ {str(current.value)} }} -> "
+            current = current.next
+        return input_ + "NULL"
+
+    def insert(self, value):
+        self.head = Node(value, self.head)
+
+    def includes(self, value):
+        current = self.head
+
+        while current:
+            if current.value == value:
+                return True
+
+            current = current.next
+
+        return False
+
+    def append(self, new):
+        new_node = Node(new)
+
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+        return new_node
+
+    def insert_before(self, value, new_value):
+        previous = current = self.head
+
+        if self.head is None:
+            raise TargetError
+        if self.includes(value) is False:
+            raise TargetError
+
+        while current:
+            if value == current.value:
+                new_node = Node(new_value)
+                if current == self.head:
+                    new_node.next = self.head
+                    self.head = new_node
+                    return new_node
+                else:
+                    previous.next = new_node
+                    new_node.next = current
+                    return new_node
+            else:
+                previous = current
+                current = current.next
+
+    def insert_after(self, value, new):
+        current = self.head
+
+        if self.head is None:
+            raise TargetError
+        if self.includes(value) is False:
+            raise TargetError
+        while current:
+            if current.value == value:
+                new_node = Node(new)
+                new_node.next = current.next
+                current.next = new_node
+                return new_node
+            else:
+                current = current.next
+
+    def length_of_linked_list(self):
+        n = 0
+        current = self.head
+
+        while current:
+            current = current.next
+            n += 1
+        return n
+
+    def kth_from_end(self, k):
+        if k >= 0:
+            current = self.head
+            length = self.length_of_linked_list()
+            target = length - k
+
+            if target < 1:
+                raise TargetError
+            for i in range(1, target):
+                current = current.next
+            return current.value
+        else:
+            raise TargetError
+
+
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next = next_
+
+
+class TargetError(Exception):
     pass
--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,3 +1,6 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
+
 class Queue:
     """
     Put docstring here
@@ -5,8 +8,39 @@ class Queue:
 
     def __init__(self):
         # initialization here
-        pass
+        self.front = None
+        self.rear = None
 
-    def some_method(self):
+    def enqueue(self, value):
         # method body here
-        pass
+        if not self.front:
+            self.front = Node(value)
+            self.rear = self.front
+        else:
+            self.rear.next = Node(value)
+            self.rear = self.rear.next
+
+    def dequeue(self):
+        if self.front is None:
+            raise InvalidOperationError
+        temporary_value = self.front
+        self.front = self.front.next
+        temporary_value.next = None
+        return temporary_value.value
+
+    def peek(self):
+        if not self.front:
+            raise InvalidOperationError
+        else:
+            return self.front.value
+
+    def is_empty(self):
+        if self.front is None:
+            return True
+        return False
+
+
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next = next_
