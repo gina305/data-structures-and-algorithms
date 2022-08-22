ce821f6b51ae17f3201a5add2ce581cc466b86b0
gina305 <napier.gina@gmail.com>
2022-08-22 10:47:30 -0400

    Added BST. Not all tests passing

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,70 +1,102 @@
-from data_structures.binary_tree import BinaryTree
-
-def create_tree():
-    tree = BinaryTree(2)
-
-    print(tree.data)
-    
-class BinarySearchTree(BinaryTree):
-    """
-Create a Binary Search Tree class
-This class should be a sub-class (or your languages equivalent) of the Binary Tree Class, with the following additional methods:
-Add:
-Arguments: value
-Return: nothing
-Adds a new node with that value in the correct location in the binary search tree.
-
-Contains:
-Argument: value
-Returns: boolean indicating whether or not the value is in the tree at least once.
-    """
-
-   
-    def __init__(self, data=None):
-        self.root = data
-        self.left = None
-        self.right = None
-        self.value = "word"
-        return self.root     
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next = next_
         
-    def add(self, word):
-        # if self.left == None:
-        #     self.left = BinaryTree(word)
-        # # else:
-        # #     node_data = BinaryTree(value)
-        # #     node_data.left = self.left
-        # #     self.left = node_data
-        # # return self.root
-        # print(self.left)
-        # return self.left
-        word = word.split()
+class BinaryTree:
 
-        return word
 
-    def insert_right(self, value):
-        if self.right == None:
-            self.right = BinaryTree(value)
-        else:
-            node_data = BinaryTree(value)
-            node_data.right = self.right
-            self.right = node_data
-        return self.right
-   
 
-    # def get_left(self):
-    #     return self.left
 
-    # def get_right(self):
-    #     return self.right
+    def __init__(self, root=None):
+        self.root = root
 
-    # def set_left(self, tree):
-    #     self.left = tree
+    def pre_order(self):
 
-    # def set_right(self, tree):
-    #     self.right = tree
+        # empty list for values
+        values = []
 
-    # def set_data(self, data):
-    #     self.data = data
+        def walk(root):
+            if root is None:
+                return
+            if root:
+                values.append(root.value)
+                walk(root.left)
+                walk(root.right)
 
-    # def get_data(self):
-    #     return self.data
+        walk(self.root)
+        return values
+
+    def in_order(self):
+        # left > root > right
+        values = []
+
+        if self.root == None:
+            return
+
+        def walk(node):
+            if node:
+                walk(node.left)
+                values.append(node.value)
+                walk(node.right)
+        walk(self.root)
+        return values
+
+    def post_order(self):
+        # left > right > root
+        values = []
+
+        if self.root == None:
+            return
+
+        def walk(node):
+            if node:
+                walk(node.left)
+                walk(node.right)
+                values.append(node.value)
+        walk(self.root)
+        return values
+
+    def max_tree(self):
+
+        if self.root == None:
+            raise Exception('Sorry this Tree is currently empty')
+
+        z = {'max_value': 0}
+
+        def walk(node, z):
+            if node:
+                walk(node.left,z)
+                walk(node.right,z)
+                if node.value > z['max_value']:
+                    z['max_value'] = node.value
+        walk(self.root, z)
+        return z['max_value']
+
+class BinarySearchTree(BinaryTree):
+    def add(self, value):
+        def walk(root):
+            if value < root.value:
+                if root.left is None:
+                    root.left = Node(value)
+                else:
+                    walk(root.left)
+            else:
+                if root.right is None:
+                    root.right = Node(value)
+                else:
+                    walk(root.right)
+        walk(self.root)
+
+    def contains(self, value):
+        def walk(root):
+            if root is None:
+                return False
+            elif root.value == value:
+                return True
+            elif value < root.value:
+                return walk(root.left)
+            else:
+                return walk(root.right)
+        return walk(self.root)
+
