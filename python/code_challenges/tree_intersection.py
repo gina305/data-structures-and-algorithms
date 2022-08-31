8eb04a875b35e968119a4678480bfc1694a989b9
gina305 <napier.gina@gmail.com>
2022-08-31 02:51:56 -0400

    Added feaure tasks. Review and implem binary tree before implmentating this feature.

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,10 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def tree_intersection():
+# Feature Tasks
+# Write a function called tree_intersection that takes two binary trees as parameters.
+# Using your Hashmap implementation as a part of your algorithm, return a set of values found in both trees.
+
+
+def tree_intersection(binary_tree1,binary_tree2):
     pass
